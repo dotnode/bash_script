c38a75e7171c8268a2234f982780666385341a9c
xnode <xnode@qq.com>
2022-12-03 23:34:45 +0800

    volume and run bash

--- install_nginx.sh
+++ install_nginx.sh
@@ -3,16 +3,11 @@
 #
 # 参考链接 https://blog.csdn.net/weixin_40461281/article/details/92586378
 #
-#
-#
-#
-#
-
 
 
 #运行根目录，可以传参指定
 BASE_PATH=$(cd `dirname $0`; pwd)
-V_DATA='/v_data';
+V_DATA='/volume';
 
 #卸载原有的apache nginx
 yum -y remove httpd nginx
@@ -25,7 +20,7 @@ systemctl restart docker.service
 #拉取nginx镜像
 docker pull nginx
 #创建目录
-mkdir -p ${V_DATA}/nginx/www ${V_DATA}/nginx/logs ${V_DATA}/nginx/conf
+mkdir -p ${V_DATA}/nginx/www/default ${V_DATA}/nginx/logs ${V_DATA}/nginx/conf
 #先建立nginx
 docker run --name nginx-conf -p 80:80 -d nginx
 #复制conf
@@ -35,7 +30,7 @@ docker stop nginx-conf
 #删除 nginx镜像
 docker rm nginx-conf
 #启动正式的
-docker run --name nginx -p 80:80 -d nginx  -v ${V_DATA}/nginx/www:/usr/share/nginx/html -v ${V_DATA}/nginx/conf/nginx.conf:/etc/nginx/nginx.conf -v ${V_DATA}/nginx/logs:/var/log/nginx nginx
+docker run --name nginx -p 80:80  -v ${V_DATA}/nginx/www/default:/usr/share/nginx/html -v ${V_DATA}/nginx/conf/nginx.conf:/etc/nginx/nginx.conf -v ${V_DATA}/nginx/logs:/var/log/nginx -d nginx 
 #获得
 CONTAINER_ID=$(docker ps -aqf "name=containername")
 #echo
