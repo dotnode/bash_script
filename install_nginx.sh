4ae02b382dbf0c16fa83f91e13ca6b94e031c150
xnode <xnode@qq.com>
2022-12-06 00:34:58 +0800

    更新脚本

--- install_nginx.sh
+++ install_nginx.sh
@@ -8,33 +8,41 @@ export PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~
 
 # Check if user is root
 if [ $(id -u) != "0" ]; then
-    echo "Error: You must be root to run this script!"
+    Echo_Red "Error: You must be root to run this script!"
     exit 1
-else
-    if env |grep -q SUDO; then
-        acme_sh_sudo="-f"
-    fi
 fi
 
+Get_Dist_Name
+if [ "${DISTRO}" = "unknow" ]; then
+    Echo_Red "Unable to get Linux distribution name, or do NOT support the current distribution."
+    exit 1
+fi
+
+Check_Stack
+if [[ "${Stack}" = "nginx" || "${Stack}" = "apache" ]]; then
+    Echo_Red "You have installed ${Stack}!"
+    exit 1
+fi
+
+clear
 echo "+-------------------------------------------+"
-echo "|           docker install nginx            |"
+echo "|    docker install nginx for ${DISTRO}     |"
 echo "+-------------------------------------------+"
 echo "|                                           |"
 echo "+-------------------------------------------+"
+
 
 #运行根目录，可以传参指定
 BASE_PATH=$(cd `dirname $0`; pwd)
 V_DATA='/volume';
 
-#卸载原有的apache nginx
-yum -y remove httpd nginx
 
 #安装docker
 if [ $(docker -h) ]; then
 	#echo
-	echo "docker nginx 已安装"
+	echo "docker 已安装"
 else
-    yum -y install docker
+    curl -fsSL https://get.docker.com | bash -s docker
 fi
 
 #开机启动
@@ -57,7 +65,7 @@ docker stop nginx-conf && docker rm nginx-conf
 #index.html
 # echo "<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>docker nginx</title></head><body><h1><center>docker nginx success!</center></h1><p><center>success!</center></p></body></html>" > /v_data/nginx/www/default/index.html
 #stop nginx #删除 nginx镜像 先删除
-if [ $(docker ps -aqf "name=nginx") ]; then
+if [[ -s $(docker ps -aqf "name=nginx") ]]; then
 	docker stop nginx && docker rm nginx
 fi
 #启动正式的
@@ -65,9 +73,144 @@ docker run --name nginx -p 80:80 -p 443:443 -v ${V_DATA}/nginx/etc/nginx:/etc/ng
 #获得
 CONTAINER_ID=$(docker ps -aqf "name=nginx")
 
-if [ CONTAINER_ID ]; then
+if [[ -s CONTAINER_ID ]]; then
 	#echo
 	echo "docker nginx安装完成"
 else
     echo "docker nginx安装失败!"
 fi
+
+Get_Dist_Name()
+{
+    if grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+        DISTRO='CentOS'
+        PM='yum'
+        if grep -Eq "CentOS Stream" /etc/*-release; then
+            isCentosStream='y'
+        fi
+    elif grep -Eqi "Alibaba" /etc/issue || grep -Eq "Alibaba Cloud Linux" /etc/*-release; then
+        DISTRO='Alibaba'
+        PM='yum'
+    elif grep -Eqi "Aliyun" /etc/issue || grep -Eq "Aliyun Linux" /etc/*-release; then
+        DISTRO='Aliyun'
+        PM='yum'
+    elif grep -Eqi "Amazon Linux" /etc/issue || grep -Eq "Amazon Linux" /etc/*-release; then
+        DISTRO='Amazon'
+        PM='yum'
+    elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release; then
+        DISTRO='Fedora'
+        PM='yum'
+    elif grep -Eqi "Oracle Linux" /etc/issue || grep -Eq "Oracle Linux" /etc/*-release; then
+        DISTRO='Oracle'
+        PM='yum'
+    elif grep -Eqi "Red Hat Enterprise Linux" /etc/issue || grep -Eq "Red Hat Enterprise Linux" /etc/*-release; then
+        DISTRO='RHEL'
+        PM='yum'
+    elif grep -Eqi "rockylinux" /etc/issue || grep -Eq "Rocky Linux" /etc/*-release; then
+        DISTRO='Rocky'
+        PM='yum'
+    elif grep -Eqi "almalinux" /etc/issue || grep -Eq "AlmaLinux" /etc/*-release; then
+        DISTRO='Alma'
+        PM='yum'
+    elif grep -Eqi "openEuler" /etc/issue || grep -Eq "openEuler" /etc/*-release; then
+        DISTRO='openEuler'
+        PM='yum'
+    elif grep -Eqi "Anolis OS" /etc/issue || grep -Eq "Anolis OS" /etc/*-release; then
+        DISTRO='Anolis'
+        PM='yum'
+    elif grep -Eqi "Kylin Linux Advanced Server" /etc/issue || grep -Eq "Kylin Linux Advanced Server" /etc/*-release; then
+        DISTRO='Kylin'
+        PM='yum'
+    elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+        DISTRO='Debian'
+        PM='apt'
+    elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
+        DISTRO='Ubuntu'
+        PM='apt'
+    elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
+        DISTRO='Raspbian'
+        PM='apt'
+    elif grep -Eqi "Deepin" /etc/issue || grep -Eq "Deepin" /etc/*-release; then
+        DISTRO='Deepin'
+        PM='apt'
+    elif grep -Eqi "Mint" /etc/issue || grep -Eq "Mint" /etc/*-release; then
+        DISTRO='Mint'
+        PM='apt'
+    elif grep -Eqi "Kali" /etc/issue || grep -Eq "Kali" /etc/*-release; then
+        DISTRO='Kali'
+        PM='apt'
+    elif grep -Eqi "UnionTech OS" /etc/issue || grep -Eq "UnionTech OS" /etc/*-release; then
+        DISTRO='UOS'
+        if command -v apt >/dev/null 2>&1; then
+            PM='apt'
+        elif command -v yum >/dev/null 2>&1; then
+            PM='yum'
+        fi
+    elif grep -Eqi "Kylin Linux Desktop" /etc/issue || grep -Eq "Kylin Linux Desktop" /etc/*-release; then
+        DISTRO='Kylin'
+        PM='yum'
+    else
+        DISTRO='unknow'
+    fi
+    Get_OS_Bit
+}
+
+Get_OS_Bit()
+{
+    if [[ `getconf WORD_BIT` = '32' && `getconf LONG_BIT` = '64' ]] ; then
+        Is_64bit='y'
+        ARCH='x86_64'
+        DB_ARCH='x86_64'
+    else
+        Is_64bit='n'
+        ARCH='i386'
+        DB_ARCH='i686'
+    fi
+
+    if uname -m | grep -Eqi "arm|aarch64"; then
+        Is_ARM='y'
+        if uname -m | grep -Eqi "armv7|armv6"; then
+            ARCH='armhf'
+        elif uname -m | grep -Eqi "aarch64"; then
+            ARCH='aarch64'
+        else
+            ARCH='arm'
+        fi
+    fi
+}
+
+Check_Stack()
+{
+    if [[ -s /usr/local/nginx/sbin/nginx ]]; then
+        Get_Stack="nginx"
+    elif [[  -s /usr/local/apache/bin/httpd || -s /etc/init.d/httpd ]]; then
+        Get_Stack="apache"
+    else
+        Get_Stack="unknow"
+    fi
+}
+
+Color_Text()
+{
+  echo -e " \e[0;$2m$1\e[0m"
+}
+
+Echo_Red()
+{
+  echo $(Color_Text "$1" "31")
+}
+
+Echo_Green()
+{
+  echo $(Color_Text "$1" "32")
+}
+
+Echo_Yellow()
+{
+  echo $(Color_Text "$1" "33")
+}
+
+Echo_Blue()
+{
+  echo $(Color_Text "$1" "34")
+}
